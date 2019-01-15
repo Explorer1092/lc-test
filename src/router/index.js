392ea5e189d9b7e1e61252c660e9ee6acf6d557f
sunpeiwen <sunpeiwen@vipkid.com.cn>
2019-01-15 11:29:59 +0800

    删除

--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-const moduleName = '/newextension'
-const helloWorld = () => import(/* webpackChunkName: 'majorExtensionMc' */ '../components/helloWorld/HelloWorld.vue')
-export default [
-  {
-    path: moduleName + '/helloWorld',
-    component: helloWorld,
-    title: '主修拓展 - VIPKID在线少儿英语'
-  }
-]
